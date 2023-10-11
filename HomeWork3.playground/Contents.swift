d81840b6aaf17dc18ff3b62aaa23848a4701ef65
maris-rozenstoks <146951342+maris-rozenstoks@users.noreply.github.com>
2023-10-11 13:33:14 +0300

    HomeWork3
    Should work on syntax.
    Calculation inside the "For loop?"
    Multiple variables inside "Loop?"

--- HomeWork3.playground/Contents.swift
+++ HomeWork3.playground/Contents.swift
@@ -4,7 +4,16 @@
  for _ in 1...period{
  }
  print("Amount of income after \(period) years will be \(profit) Eur. My total deposit will be \(deposit) Eur !")
+
  */
+import Foundation
+
+var amount = 500000.0
+var rate = 7.0
+var period = 5.0
+var deposit = amount * pow((1 + rate/100), period)
+var profit = deposit - amount
+    print("Amount of income after \(period) years will be \(profit) Eur. My total deposit will be \(deposit) Eur !")
 
 /*
  Exercise 2
@@ -12,13 +21,38 @@
  Use a % inside the for loop.
  */
 
+import UIKit
+var arrayofInt = [Int]()
+arrayofInt = [3, 5, 6, 7, 8, 8, 9]
+for evenNumber: Int in arrayofInt where evenNumber % 2 == 0 {
+    print("My even numbers are: \(evenNumber)")
+}
+
 /*
  Exercise 3
  Inside the for loop create randomNumber for the random Int calculation. Calculate and print("Number 5 will be after \(counter) shuffles"). Don't forget to make a break inside the if statement.
  */
+
+import UIKit
+var counter = 0
+var randomInts: [Int] = []
+for _ in 0...9 {
+    let randomNumber = Int.random(in: 1...20)
+    randomNumber.
+    }
+    print("Number 5 will be after \(counter) shuffles")
 
 
 /*
  Exercise 4
  A bug is climbing to a 10-meter electric post. During the day, bug can climb two meters, during the night she slides down to 1 meter. Determine with the help of the cycle how many days bug will climb on the top of the post. Think about which loop to use in which situation. print("bug will spend \(numberOfDays)) to reach top of the post")
  */
+
+var numberOfDays = 0
+while true {
+    numberOfDays += 1
+    if numberOfDays == 10 {
+        print("bug will spend \(numberOfDays) days to reach top of the post")
+        break
+    }
+}
--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,15 +1,22 @@
-import UIKit
-let dateFormatter = DateFormatter()
-dateFormatter.dateFormat = "dd/mm/yyyy"
-let birthDay = dateFormatter.date (from: "29/05/1985") ?? Date()
-let currentDay = Date()
+/*
+ Exercise 1
+ The user opens a deposit in the bank for 5 years in the amount of 500_000 Eur. The interest rate per annum \(rate). Calculate the amount of income \(profit) at the end of the \(period).
+ for _ in 1...period{
+ }
+ print("Amount of income after \(period) years will be \(profit) Eur. My total deposit will be \(deposit) Eur !")
 
-let calendar = Calendar.current
-let totalDaysFromBirth = calendar.dateComponents([.day], from: birthDay, to: currentDay).day
-let totalMonthFromBirth = calendar.dateComponents([.month], from: birthDay, to: currentDay).month
-let totalYearsFromBirth = calendar.dateComponents([.year], from: birthDay, to: currentDay).year
-if totalDaysFromBirth != 0 || totalMonthFromBirth != 0 || totalYearsFromBirth != 0 {
-    print("Total years: \(totalYearsFromBirth) , total months: \(totalMonthFromBirth), total days: \(totalDaysFromBirth) have passed")
-} else {
-    print("Wrong input!")
+
+import Foundation
+import UIKit
+var amount = 500000.0
+var rate = 7.0
+var period = 5.0
+var deposit = amount * pow((1 + rate/100), period)
+var profit = deposit - amount
+    print("Amount of income after \(period) years will be \(profit) Eur. My total deposit will be \(deposit) Eur !")
+ */
+var amount = 500000
+for _ in 1...period{
+    
 }
+print("Amount of income after \(period) years will be \(profit) Eur. My total deposit will be \(deposit) Eur !")
